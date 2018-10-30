6b5ee406934d4e3957f720ca6e0ddbf345936a1a
Bartosz Ociepka <bociepka1@gmail.com>
2018-10-30 12:36:20 +0100

    MovieSub - file length issue

---
+++ src/lab4_2/zad3/WrongFileFormatException.java
@@ -0,0 +1,7 @@
+package lab4_2.zad3;
+
+public class WrongFileFormatException extends Exception {
+    WrongFileFormatException(String a){
+        System.out.print(a);
+    }
+}
---
+++ src/lab4_2/zad3/WrongFramesException.java
@@ -0,0 +1,7 @@
+package lab4_2.zad3;
+
+class WrongFramesException extends Exception{
+    WrongFramesException(String a){
+        System.out.print(a);
+    }
+}
